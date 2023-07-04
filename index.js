682e9d2dbadfc9d7cd7121e4b3bb39dbf91e12fd
Stanislav <Stas.zaliiski@gmail.com>
2023-07-04 16:33:43 +0200

    main

---
+++ index.js
@@ -0,0 +1,27 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const contacts = require("./db/contacts");
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    case "remove":
+      const removeContacts = await contacts.removeContact(id);
+      return console.log(removeContacts);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
---
+++ readme.txt
@@ -0,0 +1,4 @@
+![action-add](./img/action-add.png)
+![action-get](./img/action-get.png)
+![action-list](./img/action-list.png)
+![action-remove](./img/action-remove.png)
